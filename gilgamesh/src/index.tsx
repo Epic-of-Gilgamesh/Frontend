29a8954bb187a9f3c630fe16f9d23c4d59b233f6
jjack27 <yizhouzhao27@gmail.com>
2022-03-19 22:18:49 +0800

    init push

---
+++ gilgamesh/src/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {LoginPage} from "./views/Login";
+import store from './store';
+import { Provider } from 'react-redux';
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Provider store={store}>
+            <Routes>
+                <Route path="/" element={<App />}> </Route>
+                <Route path="/login" element={<LoginPage title="This is the title" />}> </Route>
+            </Routes>
+        </Provider>
+    </BrowserRouter>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
